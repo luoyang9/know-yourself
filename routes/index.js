c66af491a249ff231f951fd86f454e691651fee5
Charlie Zhang <peidong97@yahoo.ca>
2016-08-21 09:28:30 -0400

    minor text fixes pt 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: "FB n' Me" });
+  res.render('home', { title: "Know Yourself" });
 });
 
 module.exports = router;
